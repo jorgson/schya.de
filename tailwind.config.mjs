adc191984dc1f2b2d755c3986366557124eb0e5e
Jörg Steinsträter <jorgson@protonmail.com>
2024-11-05 20:55:23 +0100

    herosection

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,16 @@ export default {
 	theme: {
 		extend: {
       colors: {
-        'woodland': '#3d642b'
+        'woodland': '#3d642b',
+        'woodland-darker': '#2a461e',
+        'woodland-light': '#2b6442',
+        'woodland-brown': '#5c642b',
+        'woodland-brown-dark': '#40461e',
+        'light-green': '#e1f0d6',
+        'muted-yellow': '#c9b16b',
+        'soft-beige': '#eae1d0',
+        'dark-grey': '#2b2b2b',
+
       }
     },
 	},
